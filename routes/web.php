250b141883fb6074f848ea09b9147e8647e72e20
arfan-lh <abdullaharfan775@gmail.com>
2026-04-17 15:33:20 +0700

    fix route & add migrate

--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,16 @@
 use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ContactController;
+use Illuminate\Support\Facades\Artisan;
 
 Route::get('/', [HomeController::class, 'index'])->name('home');
 
 Route::get('/project/{id}', [HomeController::class, 'show'])->name('project.show');
 
 Route::post('/contact-send', [ContactController::class, 'store'])->name('contact.store');
+
+// ROUTE MIGRATE SEMENTARA
+Route::get('/migrate', function () {
+    Artisan::call('migrate');
+    return 'Migration berhasil';
+});
