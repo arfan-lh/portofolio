8debfe371539c651234863f183154a40fa721e8f
arfan-lh <abdullaharfan775@gmail.com>
2026-04-21 15:55:02 +0700

    fix https final

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,24 +4,24 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\URL;
+use Illuminate\Http\Request;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     */
     public function register(): void
     {
         //
     }
 
-    /**
-     * Bootstrap any application services.
-     */
     public function boot(): void
     {
         if (app()->environment('production')) {
             URL::forceScheme('https');
+
+            Request::setTrustedProxies(
+                ['*'],
+                Request::HEADER_X_FORWARDED_ALL
+            );
         }
     }
 }
